e40db35235e1433c0e64a4f3150556ffecdabd48
SCOR-TELEMED\lmmiralles <luis.miralles@remarkgroup.com>
2021-12-15 11:32:07 +0100

    Updated build configuration

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,4 @@
-grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
@@ -12,12 +12,12 @@ grails.project.fork = [
         // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
         //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
 
-        // /*
+        ///*
         // configure settings for the test-app JVM, uses the daemon by default
         test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
         // configure settings for the run-app JVM
         run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-        // */
+        //*/
         // configure settings for the run-war JVM
         /* TODO: PARA DEPURAR DESCOMENTAR ESTE BLOQUE Y COMENTAR EL SUPERIOR
         test: false,
