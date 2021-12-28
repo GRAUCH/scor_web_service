3b4bc77fec0b3a389e8f4736390940816f39d4da
SCOR-TELEMED\lmmiralles <luis.miralles@remarkgroup.com>
2021-12-28 16:16:30 +0100

    Unused comments have been removed.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,31 +1,14 @@
 dataSource {
     pooled = true
     driverClassName = "com.mysql.jdbc.Driver"
-    //dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-    //dialect = org.hibernate.dialect.MySQLDialect
 	jndiName = "java:jboss/datasources/MySqlscorwsDS"
 }
 
 dataSource_CRMDynamics {
     pooled = true
     driverClassName = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-    //dialect = org.hibernate.dialect.SQLServer2008Dialect
-    //driverClassName = "com.mysql.jdbc.Driver"
-    //dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-    //dialect = org.hibernate.dialect.SQLServer2008Dialect
 	jndiName = "java:jboss/datasources/SqlCRMDynamics"
 }
-
-/*beans {
-    cacheManager {
-        //cacheManagerName = 'springSecurityCacheManager'
-        shared = true
-    }
-
-    /*ehcacheCacheManager {
-        shared = true
-    }
-}*/
 
 beans {
 	cacheManager {
@@ -36,14 +19,8 @@ beans {
 hibernate {
     cache.use_second_level_cache=true
     cache.use_query_cache=true
-    //cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
-    //flush.mode = 'manual' // This was always 'manual' in 2.4.3 not explicitly configured so.
     cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
     use_outer_join = true
-//    query.factory_class = 'org.hibernate.hql.internal.classic.ClassicQueryTranslatorFactory'
-   // cache.provider_class = 'org.hibernate.cache.EhCacheProvider'
- 	//cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory'
-// 	cache.region.factory_class = 'grails.plugin.cache.ehcache.hibernate.BeanEhcacheRegionFactory4'
 	singleSession = true
 }
 
@@ -71,7 +48,6 @@ environments {
             username = "sa"
             password = "xY;;#MID!c!572"
             url = "jdbc:sqlserver://172.17.0.36:1433;databaseName=ScorTelemed_MSCRM"
-            //url = "jdbc:mysql://172.17.0.36:1433;databaseName=ScorTelemed_MSCRM"
             logSql = true
             readOnly = true
             properties {
